525dbee17db5a1b40b354248094b94d73e451866
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-09-11 00:38:34 +0800

    Update class-haiplugin_wp_functions.php

--- includes/class-haiplugin_wp_functions.php
+++ includes/class-haiplugin_wp_functions.php
@@ -24,14 +24,14 @@ function haiplugin_wp_lang_detection_settings_page_content() {
 }
 
 // Add plugin settings fields
+add_action('admin_init', 'haiplugin_wp_lang_detection_settings_fields');
 function haiplugin_wp_lang_detection_settings_fields() {
     // Example: Add the "Enable Plugin" setting field
     register_setting('haiplugin_wp_lang_detection_group', 'haiplugin_wp_lang_detection_settings');
-
     add_settings_section('haiplugin_wp_lang_detection_general_section', 'General Settings', null, 'haiplugin_wp_lang_detection_field');
-    add_settings_field('haiplugin_wp_lang_detection_enabled', 'Enable Plugin', 'haiplugin_wp_lang_detection_enabled_callback', 'haiplugin_wp_lang_detection_field', 'propfirm_ftplugin_general_section');
+    add_settings_field('haiplugin_wp_lang_detection_enabled', 'Enable Plugin', 'haiplugin_wp_lang_detection_enabled_callback', 'haiplugin_wp_lang_detection_field', 'haiplugin_wp_lang_detection_general_section');
 }
-add_action('admin_init', 'haiplugin_wp_lang_detection_settings_fields');
+
 
 // Render "Enable Plugin" field
 function haiplugin_wp_lang_detection_enabled_callback() {
